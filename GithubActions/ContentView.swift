25fb9e6ff98b6df32403355f3edae282d289cb6d
Mark Norgren <mark@marknorgren.com>
2020-05-23 20:43:20 -0500

    Initial Xcode project

---
+++ GithubActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubActions
+//
+//  Created by Mark Norgren on 5/23/20.
+//  Copyright © 2020 Mark Norgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
